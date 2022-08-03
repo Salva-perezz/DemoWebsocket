df75ca4dbea0f460216bcff0c2fab5bab193cec6
Salvador <69496754+Salva-perezz@users.noreply.github.com>
2022-08-03 10:27:47 -0300

    Add files via upload

---
+++ public/cliente.socket.js
@@ -0,0 +1,36 @@
+const socket = io();
+const messageForm = document.querySelector("#messageForm");
+const usernameInput = document.querySelector("#usernameInput");
+const messageInput = document.querySelector("#messageInput");
+const messagesPool = document.querySelector("#messagesPool");
+
+function sendMessage(messageInfo) {
+  socket.emit("client:message", messageInfo);
+}
+
+function renderMessages(messagesInfo) {
+  const html = messagesInfo
+    .map((msgInfo) => {
+      return `<div>
+                <strong>${msgInfo.username}</strong>:
+                <em>${msgInfo.message}</em> </div>`;
+    })
+    .join(" ");
+
+  messagesPool.innerHTML = html;
+}
+
+function submitHandler(event) {
+  event.preventDefault();
+
+  const messageInfo = {
+    username: usernameInput.value,
+    message: messageInput.value,
+  };
+
+  sendMessage(messageInfo);
+}
+
+messageForm.addEventListener("submit", submitHandler);
+
+socket.on("server:messages", renderMessages);
